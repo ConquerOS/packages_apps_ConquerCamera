aec6a64dfbe319011bf3d20cb7efe487e50228ba
Jyotiraditya <jyotiraditya@aospa.co>
2020-06-24 07:23:02 +0200

    ConquerCamera: Hide Filter mode switcher button
    This doesn't work and doesn't add value even if it did work.
    Post-processing filters like this can be applied in social media
    apps or galleries.
    
    Signed-off-by: Jyotiraditya <jyotiraditya@aospa.co>
    Signed-off-by: Jake Weinstein <jake@aospa.co>
    Change-Id: Ie7a9136d9a1376f71bde594b25e699de3a30e1d4
    Signed-off-by: HeroBuxx <herobuxx@gmail.com>

--- src/com/android/camera/CaptureUI.java
+++ src/com/android/camera/CaptureUI.java
@@ -1111,13 +1111,13 @@ public class CaptureUI implements FocusOverlayManager.FocusUI,
     }
 
     public void initFilterModeButton() {
-        mFilterModeSwitcher.setVisibility(View.INVISIBLE);
+        //mFilterModeSwitcher.setVisibility(View.INVISIBLE);
         String value = mSettingsManager.getValue(SettingsManager.KEY_COLOR_EFFECT);
         if (value == null) return;
 
         enableView(mFilterModeSwitcher, SettingsManager.KEY_COLOR_EFFECT);
 
-        mFilterModeSwitcher.setVisibility(View.VISIBLE);
+        //mFilterModeSwitcher.setVisibility(View.VISIBLE);
         mFilterModeSwitcher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -1128,6 +1128,7 @@ public class CaptureUI implements FocusOverlayManager.FocusUI,
                 hideZoomSeekBar();
             }
         });
+        mFilterModeSwitcher.setVisibility(View.GONE);
     }
 
     private void enableView(View view, String key) {
@@ -1320,7 +1321,7 @@ public class CaptureUI implements FocusOverlayManager.FocusUI,
         mSceneModeLabelRect.setVisibility(View.INVISIBLE);
         mDeepZoomModeRect.setVisibility(View.INVISIBLE);
         mFrontBackSwitcher.setVisibility(View.INVISIBLE);
-        mFilterModeSwitcher.setVisibility(View.INVISIBLE);
+        //mFilterModeSwitcher.setVisibility(View.INVISIBLE);
         mSceneModeSwitcher.setVisibility(View.INVISIBLE);
         mSettingsIcon.setVisibility(View.INVISIBLE);
         String value = mSettingsManager.getValue(SettingsManager.KEY_MAKEUP);
@@ -1346,7 +1347,7 @@ public class CaptureUI implements FocusOverlayManager.FocusUI,
             mShutterButton.setVisibility(View.INVISIBLE);
             mModeSelectLayout.setVisibility(View.VISIBLE);
         }
-        mFilterModeSwitcher.setVisibility(View.VISIBLE);
+        //mFilterModeSwitcher.setVisibility(View.VISIBLE);
         if (mFilterMenuStatus == FILTER_MENU_ON) {
             removeFilterMenu(true);
         }
@@ -1363,7 +1364,7 @@ public class CaptureUI implements FocusOverlayManager.FocusUI,
         //settings for each mode
         switch (mode) {
             case DEFAULT:
-                mFilterModeSwitcher.setVisibility(View.VISIBLE);
+                //mFilterModeSwitcher.setVisibility(View.VISIBLE);
                 mSceneModeSwitcher.setVisibility(View.VISIBLE);
                 mVideoButton.setVisibility(View.INVISIBLE);
                 mMuteButton.setVisibility(View.INVISIBLE);
@@ -1371,7 +1372,7 @@ public class CaptureUI implements FocusOverlayManager.FocusUI,
                 break;
             case RTB:
             case SAT:
-                mFilterModeSwitcher.setVisibility(View.VISIBLE);
+                //mFilterModeSwitcher.setVisibility(View.VISIBLE);
                 mSceneModeSwitcher.setVisibility(View.VISIBLE);
                 mVideoButton.setVisibility(View.INVISIBLE);
                 mFlashButton.setVisibility(View.INVISIBLE);
@@ -1384,11 +1385,11 @@ public class CaptureUI implements FocusOverlayManager.FocusUI,
             case VIDEO:
             case HFR:
                 mVideoButton.setVisibility(View.VISIBLE);
-                mFilterModeSwitcher.setVisibility(View.VISIBLE);
+                //mFilterModeSwitcher.setVisibility(View.VISIBLE);
                 mShutterButton.setVisibility(View.INVISIBLE);
                 break;
             case PRO_MODE:
-                mFilterModeSwitcher.setVisibility(View.INVISIBLE);
+                //mFilterModeSwitcher.setVisibility(View.INVISIBLE);
                 mVideoButton.setVisibility(View.INVISIBLE);
                 mFrontBackSwitcher.setVisibility(View.INVISIBLE);
                 mMuteButton.setVisibility(View.INVISIBLE);
@@ -1401,9 +1402,9 @@ public class CaptureUI implements FocusOverlayManager.FocusUI,
         if (value == null) {
             mFrontBackSwitcher.setVisibility(View.INVISIBLE);
         }
-        if(mModule.mMFNREnable && mModule.getMainCameraId() ==  android.hardware.Camera.CameraInfo.CAMERA_FACING_FRONT){
+        /*if(mModule.mMFNREnable && mModule.getMainCameraId() ==  android.hardware.Camera.CameraInfo.CAMERA_FACING_FRONT){
             mFilterModeSwitcher.setVisibility(View.INVISIBLE);
-        }
+        }*/
     }
 
     public void addFilterMode() {
